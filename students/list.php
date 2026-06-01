c909f62867d8fed83e60f6eb9da93ec7b3a3067a
Karyadev88 <karyadeveloper88@gmail.com>
2026-06-01 17:26:43 +0800

    list.php update

---
+++ students/list.php
@@ -0,0 +1,36 @@
+<?php
+require_once '../db.php';
+
+$stmt = $pdo->query("SELECT * FROM students");
+$students = $stmt->fetchAll();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Students</title>
+</head>
+<body>
+    <h1>All Students</h1>
+    <a href="add.php">Add New Student</a>
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Actions</th>
+        </tr>
+        <?php foreach ($students as $student): ?>
+        <tr>
+            <td><?= $student['student_id'] ?></td>
+            <td><?= $student['name'] ?></td>
+            <td><?= $student['email'] ?></td>
+            <td>
+                <a href="edit.php?id=<?= $student['student_id'] ?>">Edit</a>
+                <a href="delete.php?id=<?= $student['student_id'] ?>">Delete</a>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+</html>
